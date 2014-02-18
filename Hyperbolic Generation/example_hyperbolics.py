951ed567ee42f6d0632c98501f07d85f00f56192
LitoNico <lito.nicolai@gmail.com>
2014-02-18 14:25:27 -0800

    finished cparse; realized that the C io functions make it useless. To be replaced by a much simpler module in the next commit

--- Hyperbolic Generation/example_hyperbolics.py
+++
@@ -1,6 +0,0 @@
-"""
-A set of examples (to be passed as the second argument of 
-'make_hyperbolic') of certain kinds of hyperbolic planes.
-""" 
-
-symmetric_hyperbolic_plane = [2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7]
