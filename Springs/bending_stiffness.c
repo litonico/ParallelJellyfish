a580eb1d15da73d2f240b5fef4d0d3fe6b6c9b0b
LitoNico <lito.nicolai@gmail.com>
2014-03-05 13:10:43 -0800

    Began blocking out bending stiffness function

---
+++ Springs/bending_stiffness.c
@@ -0,0 +1,13 @@
+// Compute faces from edges
+// Compute list of triangle pairs
+
+// Bending stiffness
+//
+// Compute each vertex normal
+//
+// For each triangle-pair, with outside verts A and B
+// and inside verts C and D:
+//      alpha_A = N(B)/(N(A) + N(B))
+//      alpha_B = N(A)/(N(A) + N(B))
+//      alpha_C = N(D)/(N(C) + N(D))
+//      alpha_D = N(C)/(N(C) + N(D))
