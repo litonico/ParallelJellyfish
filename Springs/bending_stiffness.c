512471d16f81489e691ebf8c4a8d6af7d9702ed6
LitoNico <lito.nicolai@gmail.com>
2014-03-10 15:41:42 -0700

    More work on bending stiffness

--- Springs/bending_stiffness.c
+++ Springs/bending_stiffness.c
@@ -3,40 +3,43 @@
 
 // Bending stiffness
 //
-// void precompute_stiffness
-// Compute each vertex normal in 'resting' position
-//
-// A_normal = v_cross(v_sub(A, C)
-//
-// For each triangle-pair, with outside verts A and B
-// and inside verts C and D:
-//      N_A = v_cross(v_sub(A, C), v_sub(A, D));
-//      N_B = v_cross(v_sub(B, D), v_sub(B, C));
-//      N_C = v_cross(v_sub(C, B), v_sub(C, A));
-//      N_D = v_cross(v_sub(D, A), v_sub(D, B));
-//      alpha_A = N(B)/(N(A) + N(B))
-//      alpha_B = N(A)/(N(A) + N(B))
-//      alpha_C = N(D)/(N(C) + N(D))
-//      alpha_D = N(C)/(N(C) + N(D))
-//
-// Evaluate lambda
-// l = 
+#include "bending_stiffness.h"
+
+void precompute_stiffness(FacePair facepairs[]){
+    
+    // populate array of FacePairs
+    // with appropriate data
+
+    for (int i = 0; i < num_facepairs; i++){
+        N_A = v_magnitude(v_cross(v_sub(A, C), v_sub(A, D)));
+        N_B = v_magnitude(v_cross(v_sub(B, D), v_sub(B, C)));
+        N_C = v_magnitude(v_cross(v_sub(C, B), v_sub(C, A)));
+        N_D = v_magnitude(v_cross(v_sub(D, A), v_sub(D, B)));
+        facepairs->alpha_A = N(B)/(N(A) + N(B))
+        facepairs->alpha_B = N(A)/(N(A) + N(B))
+        facepairs->alpha_C = N(D)/(N(C) + N(D))
+        facepairs->alpha_D = N(C)/(N(C) + N(D))
+
+        // Evaluate lambda
+        facepairs->l = (2.0/3.0) * (N_A + N_B)/((N_A*N_B)*(N_A*N_B))
+    }
+}
+
 // Evaluate the jacobian contribution
 //
 //
-// void runtime_stiffness(void) {
-// 
-// Evaluate bending vector R
-
-vector bending_vector = v_add(
-    v_add(
-        scalar_mul(A, alpha_A)
-        scalar_mul(B, alpha_B)
-    ),
-    v_add(
-        scalar_mul(C, alpha_C),
-        scalar_mul(D, alpha_D)
-);
+void runtime_stiffness(void) {
+    
+    // Evaluate bending vector R
+    vector bending_vector = v_add(
+        v_add(
+            scalar_mul(A, alpha_A)
+            scalar_mul(B, alpha_B)
+        ),
+        v_add(
+            scalar_mul(C, alpha_C),
+            scalar_mul(D, alpha_D)
+    );
 
 // evaluate particle forces
 // F_A
--- Springs/bending_stiffness.h
+++ Springs/bending_stiffness.h
@@ -1,12 +1,16 @@
 #ifndef bending_stiffness_h
 #define bending_stiffness_h
 
+double N_A, N_B, N_C, N_D;
+
 typedef struct {
     int A, B, C, D; // vertex numbers
     double alpha_A, alpha_B, alpha_C, alpha_D;
     double lambda;
     // Jacobian matrix?
 } FacePair;
+
+void precompute_stiffness(FacePair[] facepairs);
 
 void runtime_stiffness(FacePair[] facepairs);
 
